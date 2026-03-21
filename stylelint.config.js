61e2f31b79bad7e2903be057d6cf2083553d77a3
jana2308walter <tatjana.walter@am-stopfer.eu>
2026-03-21 21:26:48 +0100

    chore: Add prettier, eslint and stylelint

---
+++ stylelint.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: ['stylelint-scss', 'stylelint-order'],
+  rules: {
+    'scss/dollar-variable-pattern': '^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
+    'order/properties-alphabetical-order': true
+  },
+  ignoreFiles: ['**/node_modules/**', 'dist/**']
+};
