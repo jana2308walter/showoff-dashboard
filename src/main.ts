476b184cbea7e911277620a9852cf8b197d13967
jana2308walter <tatjana.walter@am-stopfer.eu>
2026-03-30 21:24:45 +0200

    chore: Lint and Fix

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,6 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { showoffDashboardConfig } from './app/showoff-dashboard.config';
 import { ShowoffDashboardComponent } from './app/showoff-dashboard.component';
 
-bootstrapApplication(ShowoffDashboardComponent, showoffDashboardConfig).catch((err) => console.error(err));
+bootstrapApplication(ShowoffDashboardComponent, showoffDashboardConfig).catch((err) =>
+  console.error(err)
+);
