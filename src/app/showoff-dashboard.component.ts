b161d776515f6c47d5ebd4fa828d993997e4f52d
jana2308walter <tatjana.walter@am-stopfer.eu>
2026-05-17 18:02:59 +0200

    chore: Add coordinate validators

--- src/app/showoff-dashboard.component.ts
+++ src/app/showoff-dashboard.component.ts
@@ -1,29 +1,47 @@
-import { Component, effect, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { WeatherService } from '../model/weather.service';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'sod-root',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './showoff-dashboard.component.html'
 })
 export class ShowoffDashboardComponent {
   private readonly weatherService = inject(WeatherService);
+  private readonly formBuilder = inject(FormBuilder);
 
   protected readonly $weather = this.weatherService.$weather;
 
-  constructor() {
-    effect(() => {
-      const weather = this.$weather.value();
+  protected readonly $currentWeather = computed(() => this.$weather.value()?.current);
+  protected readonly $hourlyWeather = computed(() => this.$weather.value()?.hourly);
+  protected readonly $dailyWeather = computed(() => this.$weather.value()?.daily);
 
-      const currentWeather = weather?.current;
-      const hourlyWeather = weather?.hourly;
-      const dailyWeather = weather?.daily;
+  protected readonly $coordinates = this.weatherService.$coordinates;
 
-      console.log('%cCurrent', 'color: fuchsia', currentWeather);
-      console.log('%cHourly', 'color: orange', hourlyWeather);
-      console.log('%cDaily', 'color: cyan', dailyWeather);
-    });
+  protected readonly form = this.formBuilder.group({
+    latitude: [47.92, [Validators.required, Validators.min(-90), Validators.max(90)]],
+    longitude: [7.79, [Validators.required, Validators.min(-180), Validators.max(180)]]
+  });
 
-    this.weatherService.coordinates = { latitude: 47.92, longitude: 7.79 };
+  private get latitudeControl(): FormControl<number> {
+    return this.form.get('latitude') as FormControl;
+  }
+
+  private get longitudeControl(): FormControl<number> {
+    return this.form.get('longitude') as FormControl;
+  }
+
+  protected checkWeather(): void {
+    const latitude = this.latitudeControl.value;
+    const longitude = this.longitudeControl.value;
+
+    const latitudeValid = this.latitudeControl.valid;
+    const longitudeValid = this.longitudeControl.valid;
+
+    if (latitudeValid && longitudeValid) {
+      this.weatherService.coordinates = { latitude, longitude };
+    }
   }
 }
--- src/model/weather.service.ts
+++ src/model/weather.service.ts
@@ -43,14 +43,31 @@ export class WeatherService {
   }
 
   readonly $weather = resource({
-    params: () => this.$coordinates(),
-    loader: async ({ params }) => {
+    params: () => {
+      const coordinates = this.$coordinates();
+      const latitude = coordinates.latitude;
+      const longitude = coordinates.longitude;
+
+      return this.coordinatesValid(latitude, longitude) ? coordinates : undefined;
+    },
+    loader: async ({ params }): Promise<Weather> => {
       return await this.getWeatherData(params.latitude, params.longitude);
     }
   }).asReadonly();
 
   private readonly range = (start: number, stop: number, step: number) =>
     Array.from({ length: (stop - start) / step }, (_, i) => start + i * step);
+
+  private coordinatesValid(latitude: number, longitude: number): boolean {
+    if (isNaN(latitude) || isNaN(longitude)) {
+      return false;
+    }
+
+    const latitudeInRange = latitude >= -90 || latitude <= 90;
+    const longitudeInRange = longitude >= -180 || longitude <= 180;
+
+    return latitudeInRange && longitudeInRange;
+  }
 
   private async getWeatherData(latitude: number, longitude: number): Promise<Weather> {
     const params = {
